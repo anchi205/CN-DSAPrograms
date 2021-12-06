77d2873730d41ede12e2418c5ac6f1c206d537c4
anchu20 <60124473+anchu20@users.noreply.github.com>
2021-12-06 11:30:34 +0530

    Update Dequeue.cpp

--- Dequeue/Dequeue.cpp
+++ Dequeue/Dequeue.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //#include "Solution.h"
 class Deque
 {
-    int  arr[100];
+    int  arr[10];
     int  front;
     int  rear;
     int  size;
@@ -18,42 +18,43 @@ class Deque
     
     bool isFull(){
         return ((front == 0 && rear == size-1)||front == rear+1);
+        // OR return (front == (rear+1)%size);
     }
     
     bool isEmpty (){                       
         return (front == -1);
     }
     
-    void insertFront(int key){            
+    void insertFront(int input){            
         if (isFull()){
             cout << -1 << endl;
             return;
         }
-        if (front == -1){                         // If queue is initially empty
+        else if (front == -1){                         // If queue is initially empty
             front = 0;
             rear = 0;
         }
-        else if (front == 0)                      // front is at first position of queue
+        else if (front == 0)                           // front is at first position of queue
             front = size - 1 ;
-        else                                      // decrement front end by '1'
-            front = front-1;
-        arr[front] = key ;                        // insert current element into Deque
+        else                                           // decrement front end by '1'
+            front = front - 1;
+        arr[front] = input ;                           // insert current element into Deque
     }
     
-    void insertRear(int key){
+    void insertRear(int input){
         if (isFull()){
             cout << -1 << endl;
             return;
         }   
-        if (front == -1){                    // If queue is initially empty
+        else if (front == -1){                         // If queue is initially empty
             front = 0;
             rear = 0;
         }    
-        else if (rear == size-1)             // rear is at last position of queue
+        else if (rear == size-1)                       // rear is at last position of queue
             rear = 0;
         else
             rear = rear+1;
-            arr[rear] = key ;                    // insert current element into Deque
+            arr[rear] = input ;                        // insert current element into Deque
     }
   
     void deleteFront(){
@@ -61,16 +62,16 @@ class Deque
             cout << -1 << endl;;
             return ;
         }
-        if (front == rear){                    // Deque has only one element
+        else if (front == rear){                    // Deque has only one element
             front = -1;
             rear = -1;
             cout << -1 << endl;
             return;
         }
-        else                                  // back to initial position
+        else                                       // back to initial position
            if (front == size -1)
                front = 0;
-           else                              // increment front by '1' to remove current front value from Deque
+           else                                    // increment front by '1' to remove current front value from Deque
                front = front+1;
     }
     
@@ -78,7 +79,7 @@ class Deque
         if (isEmpty()){
             return;
         }
-        if (front == rear){ // Deque has only one element
+        if (front == rear){                        // Deque has only one element
             front = -1;
             rear = -1;
             cout << -1 << endl;
@@ -90,14 +91,14 @@ class Deque
             rear = rear-1;
     }
   
-    int getFront(){  // Returns front element of Deque
+    int getFront(){                               // Returns front element of Deque
         if (isEmpty()){
             return -1 ;
         }
         return arr[front];
     }
 
-    int getRear(){ // function return rear element of Deque
+    int getRear(){                                // function return rear element of Deque
         if(isEmpty() || rear < 0){
             return -1 ;
         }
