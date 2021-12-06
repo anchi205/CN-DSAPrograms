4e19ee2796320baca8b5da559514f2e519e6e3f7
anchu20 <60124473+anchu20@users.noreply.github.com>
2021-12-06 11:34:42 +0530

    Add files via upload

---
+++ DeleteAlternateNodes/DeleteAlternateNodes.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+class Node {
+public:
+    int data;
+    Node * next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+    
+    ~Node() {
+        if(next) {
+            delete next;
+        }
+    }
+};
+
+using namespace std;
+//#include "solution.h"
+void deleteAlternateNodes(Node *head) {
+    if(head->next==NULL){
+        
+    }
+    Node *temp = head;
+    Node *prev = NULL;
+    int count=0;
+    while(temp!=NULL){
+        prev = temp;
+        temp = temp->next;
+        if(temp!=NULL){
+            count++;
+            if(count%2!=0){
+                prev->next = temp->next;
+            }
+        }
+    }
+}
+Node* takeinput() {
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail -> next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head) {
+    Node *temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node *head = takeinput();
+    deleteAlternateNodes(head);
+    print(head);
+    delete head;
+    return 0;
+}
