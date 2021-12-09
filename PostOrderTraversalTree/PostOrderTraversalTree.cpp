2065f0aed85f20a2fe76e033056accc56c3f27de
anchu20 <60124473+anchu20@users.noreply.github.com>
2021-12-09 23:02:38 +0530

    Update PostOrderTraversalTree.cpp

--- PostOrderTraversalTree/PostOrderTraversalTree.cpp
+++ PostOrderTraversalTree/PostOrderTraversalTree.cpp
@@ -25,11 +25,11 @@ Space complexity: O(H) -- H -- height/depth of the tree
 */
 void printPostOrder(TreeNode<int>* root) {
     if (root == NULL) {
-		return;
-	}
-	for (int i = 0; i < root->children.size(); i++) {
-		printPostOrder(root->children[i]);
-	}
+	    return;
+    }
+    for (int i = 0; i < root->children.size(); i++) {
+	    printPostOrder(root->children[i]);
+    }
     cout << root->data << " ";
 }
 TreeNode<int>* takeInputLevelWise() {
