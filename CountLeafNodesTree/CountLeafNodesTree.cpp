31d9c1931f918749c0ef9016ad9131320b2498d1
anchu20 <60124473+anchu20@users.noreply.github.com>
2021-12-09 22:34:41 +0530

    Add files via upload

---
+++ CountLeafNodesTree/CountLeafNodesTree.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+// #include "solution.h"
+int getLeafNodeCount(TreeNode<int>* root) {
+    if(root->children.size()==0){
+        return 1;            
+    }
+    int leafNodes = 0;
+    for(int i = 0;i<root->children.size();i++){
+        leafNodes += getLeafNodeCount(root->children[i]);
+    }
+    return leafNodes;
+}
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << getLeafNodeCount(root);
+}
