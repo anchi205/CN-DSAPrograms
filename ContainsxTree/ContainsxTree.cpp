62cc05fc5b6b9a466de1323a26d3c15feafff7eb
anchu20 <60124473+anchu20@users.noreply.github.com>
2021-12-10 23:34:24 +0530

    Add files via upload

---
+++ ContainsxTree/ContainsxTree.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+// #include "solution.h"
+bool isPresent(TreeNode<int>* root, int x) {
+    if (root->data == x){
+        return true;
+    }
+    bool Present;
+    for(int i = 0;i<root->children.size();i++){
+        Present = isPresent(root->children[i],x);
+    }
+    return Present;
+}
+/*
+Time complexity: O(N) -- N -- no of nodes in the tree
+Space complexity: O(H) -- H -- height/depth of the tree
+*/
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    int x;
+    cin >> x;
+    cout << (isPresent(root, x) ? "true" : "false");
+}
